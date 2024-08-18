a6b2a3dfdb36a2d65b9c529e7dc74e1bd81c0211
Strawberry <165325276+sahar-km@users.noreply.github.com>
2024-08-18 21:29:17 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1490,7 +1490,7 @@ const buildWoWOutbounds = async (env, client, remoteDNS, localDNS, blockAds, byp
                 fake_packets_delay: `${proxySettings.noiseDelayMin}-${proxySettings.noiseDelayMax}`
             };
 
-            singboxOutbound.tag = i === 1 ? `warp-ir_${index + 1}` : `Berlin ${index + 1} 🟡`;    
+            singboxOutbound.tag = i === 1 ? `warp-ir_${index + 1}` : `💦 WoW ${index + 1} 🌍`;    
             
             if (i === 0) {
                 singboxOutbound.detour = `warp-ir_${index + 1}`;
