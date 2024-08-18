7c26cb1efd2b7700747ee2dbf1f714ebfa4d2d95
Strawberry <165325276+sahar-km@users.noreply.github.com>
2024-08-18 22:35:39 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1490,10 +1490,10 @@ const buildWoWOutbounds = async (env, client, remoteDNS, localDNS, blockAds, byp
                 fake_packets_delay: `${proxySettings.noiseDelayMin}-${proxySettings.noiseDelayMax}`
             };
 
-            singboxOutbound.tag = i === 1 ? `warp-ir_${index + 1}` : `Berlin ${index + 1} 🌍`;    
+            singboxOutbound.tag = i === 1 ? `warp-ir_${index + 1}` : `Berlin ${index + 1} 🟡`;    
             
             if (i === 0) {
-                singboxOutbound.detour = `warp-ir_${index + 1}`;
+                singboxOutbound.detour = `Iran_${index + 1}`;
             } else {
                 delete singboxOutbound.detour;
             }
